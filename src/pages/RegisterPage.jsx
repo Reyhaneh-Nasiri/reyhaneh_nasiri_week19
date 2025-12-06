bf6aab6b014c8c7e4f07da043314a7242f3ca977
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-06 08:33:18 +0330

    feat(register-page): add UI

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,41 @@
-const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+import AuthForm from "@/components/AuthForm/AuthForm";
 
-export default RegisterPage
+import styles from "./RegisterPage.module.css";
+
+const registerInputs = [
+  { id: "username", name: "username", type: "text", placeholder: "نام کاربری" },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "رمز عبور",
+  },
+  {
+    id: "confirm",
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "تکرار رمز عبور",
+  },
+];
+
+const RegisterPage = () => {
+  const handleRegister = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>بوت کمپ بوتواستارت</h1>
+      <AuthForm
+        onSubmit={handleRegister}
+        title="فرم ثبت نام"
+        inputs={registerInputs}
+        button="ثبت نام"
+        redirect="حساب کاربری دارید؟"
+        path="/login"
+      />
+    </div>
+  );
+};
+
+export default RegisterPage;
