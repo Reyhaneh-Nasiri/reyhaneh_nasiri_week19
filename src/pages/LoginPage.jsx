75afcde5c603c5d150c545460780c69ae7c0715c
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-06 06:53:25 +0330

    feat(login-page): add UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,46 @@
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+import * as yup from "yup";
 
-export default LoginPage
+import AuthForm from "@/components/AuthForm/AuthForm";
+
+import styles from "./LoginPage.module.css";
+
+const loginInputs = [
+  { id: "username", name: "username", type: "text", placeholder: "نام کاربری" },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "رمز عبور",
+  },
+];
+
+const LoginPage = () => {
+  const handleLogin = (data) => {
+    console.log(data);
+  };
+
+  const loginSchema = yup.object().shape({
+    username: yup.string().required("نام کاربری الزامی است"),
+    password: yup
+      .string()
+      .required("رمز عبور الزامی است")
+      .min(4, "حداقل باید 4 کاراکتر باشد")
+      .max(20, "حداکثر 20 کاراکتر مجاز است"),
+  });
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>بوت کمپ بوتواستارت</h1>
+      <AuthForm
+        onSubmit={handleLogin}
+        title="فرم ورود"
+        inputs={loginInputs}
+        button="ورود"
+        redirect="ایجاد حساب کاربری!"
+        path="/register"
+        schema={loginSchema}
+      />
+    </div>
+  );
+};
+
+export default LoginPage;
