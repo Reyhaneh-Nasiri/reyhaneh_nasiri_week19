3bc24e0a69b1f52972b3e6cea4e4bd5cf1c913a4
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-06 23:50:49 +0330

    refactor(login-page): improve maintainability by isolating login validation schema

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,10 @@
-import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import AuthForm from "@/components/AuthForm/AuthForm";
 import { login } from "@/services/auth";
 import { setCookie } from "@/utils/cookie";
+import { loginSchema } from "@/utils/validators/loginSchema";
 
 import styles from "./LoginPage.module.css";
 
@@ -31,14 +31,6 @@ const LoginPage = () => {
     }
   };
 
-  const loginSchema = yup.object().shape({
-    username: yup.string().required("نام کاربری الزامی است"),
-    password: yup
-      .string()
-      .required("رمز عبور الزامی است")
-      .min(4, "حداقل باید 4 کاراکتر باشد")
-      .max(20, "حداکثر 20 کاراکتر مجاز است"),
-  });
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>بوت کمپ بوتواستارت</h1>
