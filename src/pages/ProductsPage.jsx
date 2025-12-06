cf39e280c2ad430e7a161005bbac90a7c0c08df8
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-06 22:11:04 +0330

    feat(products-page): add structure to the products page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,17 @@
-import SearchBar from "@/components/SearchBar/SearchBar"
+import ProductPagination from "@/components/products/ProductPagination/ProductPagination";
+import ProductsHeader from "@/components/products/ProductsHeader/ProductsHeader";
+import ProductsList from "@/components/products/ProductsList/ProductsList";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 const ProductsPage = () => {
   return (
     <div>
       <SearchBar />
+      <ProductsHeader />
+      <ProductsList />
+      <ProductPagination />
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
