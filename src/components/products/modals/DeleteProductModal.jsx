21e4e185b6811a0ec6a7a6b653ede8e4572e490e
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-07 23:50:23 +0330

    feat(delete-product-modal): add delete confirmation modal with layout and style

---
+++ src/components/products/modals/DeleteProductModal.jsx
@@ -0,0 +1,22 @@
+import closeIcon from "@/assets/icons/close.svg";
+
+import styles from "./DeleteProductModal.module.css";
+
+const DeleteProductModal = () => {
+  return (
+    <div className={styles.modal}>
+      <img src={closeIcon} alt="close modal" className={styles["modal__header-icon"]} />
+      <p className={styles.modal__description}>آیا از حذف این محصول مطمئنید؟</p>
+      <div className={styles.modal__buttons}>
+        <button type="button" className={`${styles.modal__button} ${styles["modal__button--delete"]}`}>
+          حذف
+        </button>
+        <button type="button" className={`${styles.modal__button} ${styles["modal__button--cancel"]}`}>
+          لغو
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteProductModal;
