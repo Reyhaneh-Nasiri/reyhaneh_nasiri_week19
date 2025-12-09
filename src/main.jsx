012c4cc0c2afcb09832b43c141039682022d8656
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-09 14:14:31 +0330

    chore(main): integrate ModalProvider into application bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/core/global.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ModalProvider from "./contexts/modal/ModalProvider";
+import "./styles/core/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </BrowserRouter>
   </StrictMode>
 );
