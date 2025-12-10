94891f3adf2ca5e8596a413122f3efa60ce7713d
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-10 12:51:43 +0330

    feat(form): handle and validation form by using react-hook-form and zod

--- src/components/products/ProductForm/ProductForm.jsx
+++ src/components/products/ProductForm/ProductForm.jsx
@@ -1,14 +1,22 @@
-import useModal from "@/hooks/useModal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { productSchema } from "@/utils/validators/productsSchema";
 import { INPUT_CONFIG } from "./productForm.config";
+import useModal from "@/hooks/useModal";
 import styles from "./ProductForm.module.css";
 
 const ProductForm = ({ title, confirmButton, initialValues, onSubmit }) => {
   const { closeModal } = useModal();
-  const changeHandler = (e) => {
-    console.log(e.target.name, e.target.value);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: initialValues ?? { name: "", quantity: null, price: null },
+    resolver: zodResolver(productSchema),
+  });
   return (
-    <form className={styles["product-form"]} onSubmit={onSubmit}>
+    <form className={styles["product-form"]} onSubmit={handleSubmit(onSubmit)}>
       <h3 className={styles["product-form__title"]}>{title}</h3>
 
       {/* fields */}
@@ -24,26 +32,34 @@ const ProductForm = ({ title, confirmButton, initialValues, onSubmit }) => {
 
             <input
               {...inputProps}
-              value={initialValues?.[inputProps.name] ?? ""}
-              onChange={changeHandler}
-              className={styles["product-form__input"]}
+              {...register(inputProps.name)}
+              className={`${styles["product-form__input"]} ${
+                errors[inputProps.name]
+                  ? styles["product-form__input--error"]
+                  : ""
+              }`}
             />
+            {errors[inputProps.name] && (
+              <p className={styles["product-form__error"]}>
+                {errors[inputProps.name]?.message}
+              </p>
+            )}
           </div>
         ))}
       </div>
-
       {/* actions */}
       <div className={styles["product-form__buttons"]}>
         <button
+          disabled={isSubmitting}
           type="submit"
           className={`${styles["product-form__button"]} primary-button`}
         >
-          {confirmButton}
+          {isSubmitting ? "در حال پردازش..." : confirmButton}
         </button>
         <button
           type="button"
           className={`${styles["product-form__button"]} cancel-button`}
-          onClick={() => closeModal()}
+          onClick={closeModal}
         >
           انصراف
         </button>
