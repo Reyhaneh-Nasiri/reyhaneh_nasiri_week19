47f3add1df2232e0d92aee6ce6cf520b856e6023
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-10 12:10:19 +0330

    feat(modal): open modal by click on add/edit/delete button

--- src/components/products/ProductItem/ProductItem.jsx
+++ src/components/products/ProductItem/ProductItem.jsx
@@ -1,17 +1,30 @@
 import { TbEdit, TbTrash } from "react-icons/tb";
+import useModal from "@/hooks/useModal";
 import styles from "./ProductItem.module.css";
-const ProductItem = ({ title, quantity, price, id }) => {
+const ProductItem = ({ name, quantity, price, id }) => {
+  const { openModal } = useModal();
   return (
     <li className={styles.product}>
-      <p className={styles.product__title}>{title}</p>
+      <p className={styles.product__title}>{name}</p>
       <p className={styles.product__quantity}>{quantity}</p>
       <p className={styles.product__price}>
         {price} <span>هزار تومان</span>
       </p>
       <p className={styles.product__id}>{id}</p>
       <div className={styles["product__action-group"]}>
-        <TbEdit className={styles["product__action-icon"]} />
-        <TbTrash className={styles["product__action-icon"]} />
+        <TbEdit
+          className={styles["product__action-icon"]}
+          onClick={() =>
+            openModal("edit-product", {
+              id,
+              initialValues: { name, quantity, price },
+            })
+          }
+        />
+        <TbTrash
+          className={styles["product__action-icon"]}
+          onClick={() => openModal("delete-product", { id })}
+        />
       </div>
     </li>
   );
--- src/components/products/ProductsHeader/ProductsHeader.jsx
+++ src/components/products/ProductsHeader/ProductsHeader.jsx
@@ -1,14 +1,19 @@
 import { LuSettings2 } from "react-icons/lu";
+import useModal from "@/hooks/useModal";
 import styles from "./ProductsHeader.module.css";
 
 const ProductsHeader = () => {
+  const { openModal } = useModal();
   return (
     <div className={styles["products-header"]}>
       <h2 className={styles["products-header__title"]}>
         <LuSettings2 className={styles["products-header__icon"]} />
         مدیریت کالا
       </h2>
-      <button className={`${styles["products-header__button"]} primary-button`}>
+      <button
+        className={`${styles["products-header__button"]} primary-button`}
+        onClick={() => openModal("add-product")}
+      >
         افزودن محصول
       </button>
     </div>
