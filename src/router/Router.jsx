81f4c8aae72ce3cfc26130896fb244aeeaeb8d20
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-12 21:52:21 +0330

    refactor(routing): protect products page using ProtectedRoute

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,17 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "@/pages/404";
 import LoginPage from "@/pages/LoginPage";
 import ProductsPage from "@/pages/ProductsPage";
 import RegisterPage from "@/pages/RegisterPage";
-import { Navigate, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Router = () => {
   return (
     <>
       <Routes>
-        <Route index element={<Navigate to="/login" />} />
-        <Route path="/products" element={<ProductsPage />} />
+        {/* Redirect root */}
+        <Route index element={<Navigate to="/products" />} />
+        {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/products" element={<ProductsPage />} />
+        </Route>
+        {/* 404 */}
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
