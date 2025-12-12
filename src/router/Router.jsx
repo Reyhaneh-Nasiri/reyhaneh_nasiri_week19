e5c7f1ecec2def8149001eef2b96832463e63baa
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-12 23:38:14 +0330

    perf(routing): enable lazy loading with Suspense

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,26 +1,30 @@
+import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import PageNotFound from "@/pages/404";
-import LoginPage from "@/pages/LoginPage";
-import ProductsPage from "@/pages/ProductsPage";
-import RegisterPage from "@/pages/RegisterPage";
 import ProtectedRoute from "./ProtectedRoute";
+
+const ProductsPage = lazy(() => import("@/pages/ProductsPage"));
+const RegisterPage = lazy(() => import("@/pages/RegisterPage"));
+const LoginPage = lazy(() => import("@/pages/LoginPage"));
+const PageNotFound = lazy(() => import("@/pages/404"));
 
 const Router = () => {
   return (
     <>
-      <Routes>
-        {/* Redirect root */}
-        <Route index element={<Navigate to="/products" />} />
-        {/* Public Routes */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/products" element={<ProductsPage />} />
-        </Route>
-        {/* 404 */}
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Redirect root */}
+          <Route index element={<Navigate to="/products" />} />
+          {/* Public Routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/products" element={<ProductsPage />} />
+          </Route>
+          {/* 404 */}
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
