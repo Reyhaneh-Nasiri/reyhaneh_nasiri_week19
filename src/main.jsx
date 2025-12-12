16a1d1c54855b99b0587fc4b07fb67e5ec6f8b3e
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-12-12 22:01:25 +0330

    feat(auth): wrap AuthProvider in main.jsx - enable global authentication state across the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lip/react-query.js";
 import App from "./App.jsx";
 import ModalProvider from "./contexts/modal/ModalProvider";
+import AuthProvider from "./contexts/auth/AuthProvider.jsx";
+import { queryClient } from "./lip/react-query.js";
 import "./styles/core/global.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <App />
-        </ModalProvider>
+        <AuthProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
